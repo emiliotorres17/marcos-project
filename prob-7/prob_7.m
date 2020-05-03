efc3f4143d474b778aeb2c083d353482e77185bb
emiliotorres17 <emiliotorres17@yahoo.com>
2020-05-03 00:21:15 -0700

    cleaned up code

--- prob-7/prob_7.m
+++ prob-7/prob_7.m
@@ -22,11 +22,15 @@ global alk
 global del_a
 global C0
 %-------------------------------------------------------------------------%
+% Plotting flags                                                          %
+%-------------------------------------------------------------------------%
+C_plot = 1;
+T_plot = 1;
+%-------------------------------------------------------------------------%
 % Reading in data                                                         %
 %-------------------------------------------------------------------------%
 Em                  = xlsread('../data/EmissionsProj.xlsx', 'B2:B265');
 El                  = xlsread('../data/EmissionsProj.xlsx', 'C2:C265');
-%Em                  = (Em+El)*10^15/12.0;
 Em                  = (Em)*10^15/12.0;
 El                  = (El)*10^15/12.0;
 emt                 = linspace(1751, 2014, length(Em));
@@ -58,41 +62,46 @@ k1      = 1.44e-08;
 k2      = 8.154e-12;
 alk     = 767e+15/12.0;
 del_a   = OM/(AM*(1+del_d));
-Qa      = 590*10^15/12;     
-Qu      = 713*10^15/12;     
-Ql      = 35658*10^15/12.0; 
+Qa      = 590*10^15/12;
+Qu      = 713*10^15/12;
+Ql      = 35658*10^15/12.0;
 M       = 2400*10^15/12.0;
 C_bio   = 2400*10^15/12.0;
 %-------------------------------------------------------------------------%
 % solving for CO2                                                         %
 %-------------------------------------------------------------------------%
-Qint        = [Qa; Qu; Ql; M; C_bio; 0.0; 0.0]; 
+Qint        = [Qa; Qu; Ql; M; C_bio; 0.0; 0.0];
 options     = odeset('MaxStep', 0.5);
 [t, out]    = ode45(@(t,y) func3(t, y, emt, Em, El), [1751 2014], Qint, options);
-hold on
 Qa  = out(:,1)./AM.*10^6;
-plot(t, Qa, 'r', 'linewidth', 1.5)
-plot(CO2_yrs, CO2_data, 'b--', 'linewidth', 1.5)
-legend('Model prediction', 'Historical CO$_{2}$', 'interpreter', 'latex', ...
-        'location' , 'Northwest')
-xlabel('Time [yrs]', 'interpreter', 'latex')
-ylabel('Atmospheric CO$_{2}$ [ppm]', 'interpreter', 'latex' )
-grid('on')
-xlim([1751, 2014]);
-ylim([260, 400]);
-saveas(gcf, 'prob-7-1.png')
+if C_plot == 1
+    figure
+    hold on
+    plot(t, Qa, 'r', 'linewidth', 1.5)
+    plot(CO2_yrs, CO2_data, 'b--', 'linewidth', 1.5)
+    legend('Model prediction', 'Historical CO$_{2}$', 'interpreter', 'latex', ...
+            'location' , 'Northwest')
+    xlabel('Time [yrs]', 'interpreter', 'latex')
+    ylabel('Atmospheric CO$_{2}$ [ppm]', 'interpreter', 'latex' )
+    grid('on')
+    xlim([1751, 2014]);
+    ylim([260, 400]);
+    saveas(gcf, 'prob-7-1.png')
+end
 %-------------------------------------------------------------------------%
-% Plotting temperature                                                    %    
+% Plotting temperature                                                    %
 %-------------------------------------------------------------------------%
-Tmix        = out(:,6);
-figure          
-hold on
-plot(t, Tmix, 'r', 'linewidth', 1.5)
-plot(temp_yrs, temp_data, 'b', 'linewidth', 1.5)
-legend('Model Prediction', 'Historical Temperature Anomaly', ...
-            'interpreter', 'latex', 'location' , 'Northwest')
-xlabel('Time [yrs]', 'interpreter', 'latex')
-ylabel('Temperature Anomaly [K]', 'interpreter', 'latex' )
-grid('on')
-xlim([1751, 2014])
-saveas(gcf, 'prob-7-2.png')
+if T_plot == 1
+    Tmix        = out(:,6);
+    figure
+    hold on
+    plot(t, Tmix, 'r', 'linewidth', 1.5)
+    plot(temp_yrs, temp_data, 'b', 'linewidth', 1.5)
+    legend('Model Prediction', 'Historical Temperature Anomaly', ...
+                'interpreter', 'latex', 'location' , 'Northwest')
+    xlabel('Time [yrs]', 'interpreter', 'latex')
+    ylabel('Temperature Anomaly [K]', 'interpreter', 'latex' )
+    grid('on')
+    xlim([1751, 2014])
+    saveas(gcf, 'prob-7-2.png')
+end
